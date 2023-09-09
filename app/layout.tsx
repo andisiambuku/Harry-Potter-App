477b5336937ef147617e203341d61312f7ae8b60
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2023-09-09 17:43:52 +0300

    layout file debug

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from "next/font/google";
 
 export const inter = Inter({subsets: ['latin'], });
 
+
 export const metadata: Metadata = {
   title: "Harry Potter App",
   description: "Harry Potter App",
